6c0ae85cf78789f389a90e6f499a7204f16cdba1
Florian Engelhardt <florian.engelhardt@oxid-esales.com>
2019-11-21 13:26:03 +0100

    Fix build

---
+++ metadata.php
@@ -0,0 +1,41 @@
+<?php
+
+/**
+ * Copyright © OXID eSales AG. All rights reserved.
+ * See LICENSE file for license details.
+ */
+
+/**
+ * Metadata version
+ */
+$sMetadataVersion = '2.0';
+
+/**
+ * Module information
+ */
+$aModule = [
+    'id'            => 'oe/graphql-catalogue',
+    'title'         => [
+        'de'        =>  'GraphQL Katatlog',
+        'en'        =>  'GraphQL Catalogue',
+    ],
+    'description'   =>  [
+        'de' => '<span>OXID GraphQL Katalog</span>',
+        'en' => '<span>OXID GraphQL Catalogue</span>',
+    ],
+    'thumbnail'   => 'out/pictures/logo.png',
+    'version'     => '0.1.0',
+    'author'      => 'OXID eSales',
+    'url'         => 'www.oxid-esales.com',
+    'email'       => 'info@oxid-esales.com',
+    'extend'      => [
+    ],
+    'controllers' => [
+    ],
+    'templates'   => [
+    ],
+    'blocks'      => [
+    ],
+    'settings'    => [
+    ],
+];
