5b5f00cd7cf60392738694c3a39fd60c6ccfe75e
Florian Engelhardt <florian.engelhardt@oxid-esales.com>
2020-01-16 12:39:32 +0100

    Remove not needed LegacyService

--- tests/Integration/Controller/ManufacturerTest.php
+++ tests/Integration/Controller/ManufacturerTest.php
@@ -5,7 +5,6 @@ declare(strict_types=1);
 namespace OxidEsales\GraphQL\Catalogue\Tests\Integration\Controller;
 
 use OxidEsales\GraphQL\Base\Tests\Integration\TestCase;
-use OxidEsales\GraphQL\Base\Service\LegacyServiceInterface;
 
 class ManufacturerTest extends TestCase
 {
