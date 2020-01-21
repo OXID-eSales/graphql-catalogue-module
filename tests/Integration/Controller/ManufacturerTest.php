faf4ba5a6a4fa0891009e49eb58c0eb9f3a14109
hkreuter <heike@reuter-consultig.org>
2020-01-21 10:28:27 +0100

    OXDEV-2787 Improve tests

--- tests/Integration/Controller/ManufacturerTest.php
+++ tests/Integration/Controller/ManufacturerTest.php
@@ -1,5 +1,10 @@
 <?php
 
+/**
+ * Copyright © OXID eSales AG. All rights reserved.
+ * See LICENSE file for license details.
+ */
+
 declare(strict_types=1);
 
 namespace OxidEsales\GraphQL\Catalogue\Tests\Integration\Controller;
@@ -9,14 +14,23 @@ use OxidEsales\GraphQL\Base\Tests\Integration\TestCase;
 class ManufacturerTest extends TestCase
 {
 
-    private static $ACTIVE_MANUFACTURER = "oiaf6ab7e12e86291e86dd3ff891fe40";
-    private static $INACTIVE_MANUFACTURER  = "dc50589ad69b6ec71721b25bdd403171";
-    private static $ACTIVE_MULTILANGUAGE_MANUFACTURER = 'adc6df0977329923a6330cc8f3c0a906';
+    private const ACTIVE_MANUFACTURER = "oiaf6ab7e12e86291e86dd3ff891fe40";
+    private const INACTIVE_MANUFACTURER  = "dc50589ad69b6ec71721b25bdd403171";
+
+    protected function setUp(): void
+    {
+        parent::setUp();
+
+        $this->setGETRequestParameter(
+            'lang',
+            '0'
+        );
+    }
 
     public function testGetSingleActiveManufacturer()
     {
         $result = $this->query('query {
-            manufacturer (id: "' . self::$ACTIVE_MANUFACTURER . '") {
+            manufacturer (id: "' . self::ACTIVE_MANUFACTURER . '") {
                 id
                 active
                 icon
@@ -34,7 +48,7 @@ class ManufacturerTest extends TestCase
         unset($result['body']['data']['manufacturer']['timestamp']);
         $this->assertEquals(
             [
-                'id' => self::$ACTIVE_MANUFACTURER,
+                'id' => self::ACTIVE_MANUFACTURER,
                 'active' => 1,
                 'icon' => 'oreilly_1_mico.png',
                 'title' => 'O\'Reilly',
@@ -48,7 +62,7 @@ class ManufacturerTest extends TestCase
     public function testGet401ForSingleInactiveManufacturer()
     {
         $result = $this->query('query {
-            manufacturer (id: "' . self::$INACTIVE_MANUFACTURER . '") {
+            manufacturer (id: "' . self::INACTIVE_MANUFACTURER . '") {
                 id
                 active
                 icon
@@ -169,98 +183,6 @@ class ManufacturerTest extends TestCase
         // fixtures have 0 manufacturers matching title DOES-NOT-EXIST
         $this->assertEquals(
             0,
-            count($result['body']['data']['manufacturers'])
-        );
-    }
-
-    public function providerGetManufacturerMultilanguage()
-    {
-        return [
-            'de' => [
-                'languageId' => '0',
-                'title'      => 'Liquid Force'
-            ],
-            'en' => [
-                'languageId' => '1',
-                'title'      => 'Liquid Force Kite'
-            ],
-        ];
-    }
-
-    /**
-     * @dataProvider providerGetManufacturerMultilanguage
-     */
-    public function testGetManufacturerMultilanguage(string $languageId, string $title)
-    {
-        $query = 'query {
-            manufacturer (id: "' . self::$ACTIVE_MULTILANGUAGE_MANUFACTURER . '") {
-                id
-                title
-            }
-        }';
-
-        $this->setGETRequestParameter(
-            'lang',
-            $languageId
-        );
-
-        $result = $this->query($query);
-        $this->assertResponseStatus(
-            200,
-            $result
-        );
-
-        $this->assertEquals(
-            [
-                'id' => self::$ACTIVE_MULTILANGUAGE_MANUFACTURER,
-                'title' => $title
-            ],
-            $result['body']['data']['manufacturer']
-        );
-    }
-
-    public function providerGetManufacturerListWithFilterMultilanguage()
-    {
-        return [
-            'de' => [
-                'languageId' => '0',
-                'count'      => 0
-            ],
-            'en' => [
-                'languageId' => '1',
-                'count'      => 1
-            ]
-        ];
-    }
-
-    /**
-     * @dataProvider providerGetManufacturerListWithFilterMultilanguage
-     */
-    public function testGetManufacturerListWithFilterMultilanguage(string $languageId, int $count)
-    {
-        $query = 'query{
-            manufacturers(filter: {
-                title: {
-                    contains: "Force Kite"
-                }
-            }){
-                id
-            }
-        }';
-
-        $this->setGETRequestParameter(
-            'lang',
-            $languageId
-        );
-
-        $result = $this->query($query);
-        $this->assertResponseStatus(
-            200,
-            $result
-        );
-
-        $this->assertEquals(
-            $count,
             count($result['body']['data']['manufacturers'])
         );
     }
