f2845ec11aa1b87de63dc5328dba00dba6c2d530
Anton Fedurtsya <anton@fedurtsya.com>
2020-01-30 17:28:33 +0200

    OXDEV-3042 Remove unnecessary id property and getId method from Exceptions

--- src/Exception/VendorNotFound.php
+++ src/Exception/VendorNotFound.php
@@ -13,27 +13,12 @@ use OxidEsales\GraphQL\Base\Exception\NotFound;
 
 class VendorNotFound extends NotFound
 {
-
-    /** @var string */
-    private $id;
-
     /**
      * @param string $id
      * @return self
      */
     public static function byId(string $id): self
     {
-        $ex = new self(sprintf('Vendor was not found by id: %s', $id));
-        $ex->id = $id;
-
-        return $ex;
-    }
-
-    /**
-     * @return string
-     */
-    public function id(): string
-    {
-        return $this->id;
+        return new self(sprintf('Vendor was not found by id: %s', $id));
     }
 }
