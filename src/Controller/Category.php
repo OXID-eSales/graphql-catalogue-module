6d9c00091d22010d7338f57f04f9092058f48453
Angel Dimitrov <angel.dimitrov@dnhsoft.com>
2020-01-31 19:16:39 +0200

    OXDEV-2995 Add categories query

--- src/Controller/Category.php
+++ src/Controller/Category.php
@@ -11,13 +11,17 @@ namespace OxidEsales\GraphQL\Catalogue\Controller;
 
 use OxidEsales\GraphQL\Base\Exception\InvalidLogin;
 use OxidEsales\GraphQL\Base\Exception\NotFound;
+use OxidEsales\GraphQL\Base\DataType\BoolFilter;
 use OxidEsales\GraphQL\Catalogue\DataType\Category as CategoryDataType;
 use OxidEsales\GraphQL\Catalogue\DataType\CategoryFilterList;
 use OxidEsales\GraphQL\Catalogue\Exception\CategoryNotFound;
+use OxidEsales\GraphQL\Catalogue\Service\Repository;
 use TheCodingMachine\GraphQLite\Annotations\Query;
 
 class Category extends Base
 {
+    /** @var Repository*/
+    protected $repository;
 
     /**
      * @Query()
@@ -42,5 +46,27 @@ class Category extends Base
         }
 
         return $category;
+    }
+
+    /**
+     * @Query()
+     *
+     * @return CategoryDataType[]
+     */
+    public function categories(?CategoryFilterList $filter = null): array
+    {
+        $filter = $filter ?? new CategoryFilterList();
+
+        if (!$this->isAuthorized('VIEW_INACTIVE_CATEGORY')) {
+            $filter = $filter->withActiveFilter(new BoolFilter(true));
+        }
+
+        try {
+            $categories = $this->repository->getByFilter($filter, CategoryDataType::class);
+        } catch (\Exception $e) {
+            return [];
+        }
+
+        return $categories;
     }
 }
