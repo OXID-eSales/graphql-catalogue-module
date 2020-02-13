9373f156ffd36b56f11634c9d7f0fdd519bbc0ff
hkreuter <heike@reuter-consultig.org>
2020-02-13 11:36:49 +0100

    OXDEV-2998 Merge category multt language tests into one

--- tests/Integration/Controller/CategoryMultiLanguageTest.php
+++ tests/Integration/Controller/CategoryMultiLanguageTest.php
@@ -9,11 +9,58 @@ declare(strict_types=1);
 
 namespace OxidEsales\GraphQL\Catalogue\Tests\Integration\Controller;
 
-use OxidEsales\Eshop\Core\Registry;
 use OxidEsales\GraphQL\Base\Tests\Integration\TestCase;
 
 final class CategoryMultiLanguageTest extends TestCase
 {
+    private const ACTIVE_CATEGORY = 'd86fdf0d67bf76dc427aabd2e53e0a97';
+
+    /**
+     * @dataProvider providerGetCategoryMultiLanguage
+     *
+     * @param string $languageId
+     * @param string $title
+     */
+    public function testGetCategoryMultiLanguage(string $languageId, string $title)
+    {
+        $query = 'query {
+            category (id: "' . self::ACTIVE_CATEGORY . '") {
+                id
+                title
+            }
+        }';
+
+        $this->setGETRequestParameter(
+            'lang',
+            $languageId
+        );
+
+        $result = $this->query($query);
+        $this->assertResponseStatus(200, $result);
+
+        $this->assertEquals(
+            [
+                'id' => self::ACTIVE_CATEGORY,
+                'title' => $title
+            ],
+            $result['body']['data']['category']
+        );
+    }
+
+    public function providerGetCategoryMultiLanguage()
+    {
+        return [
+            'de' => [
+                'languageId' => '0',
+                'title'      => 'Schuhe'
+            ],
+            'en' => [
+                'languageId' => '1',
+                'title'      => 'Shoes'
+            ],
+        ];
+    }
+
     /**
      * @dataProvider providerGetCategoryListWithFilterMultiLanguage
      *
