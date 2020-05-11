3ca5fc55ceaf64213b2f00b7152c1c91d84a2fd4
Florian Engelhardt <florian.engelhardt@oxid-esales.com>
2020-05-11 14:22:00 +0200

    OXDEV-3471 move fixture from EE to CE

--- tests/Fixtures/testdemodata_ee.sql
+++ tests/Fixtures/testdemodata_ee.sql
@@ -15,12 +15,6 @@ INSERT INTO `oxlinks2shop` (`OXSHOPID`, `OXMAPOBJECTID`, `OXTIMESTAMP`) VALUES
 (1,	904, '2020-04-16 16:41:05'),
 (1,	905, '2020-04-16 16:41:05');
 
-# Banner is configured for group
-INSERT INTO `oxactions` (`OXID`, `OXSHOPID`, `OXTYPE`, `OXSORT`, `OXACTIVE`) VALUES
-('_test_group_banner', 1, 3, 6, 1);
-INSERT INTO `oxobject2action` (`OXACTIONID`, `OXOBJECTID`, `OXCLASS`) VALUES
-('_test_group_banner','oxidadmin', 'oxgroups');
-
 # Banner for second shop
 INSERT INTO `oxactions` (`OXID`, `OXSHOPID`, `OXTYPE`, `OXSORT`, `OXACTIVE`, `OXTITLE`) VALUES
 ('_test_second_shop_banner_1', 2, 3, 2, 1, 'subshop banner 1'),
