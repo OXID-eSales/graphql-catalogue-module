b1d46767f308fccdda527bbb410b04356776fbf6
Florian Engelhardt <florian.engelhardt@oxid-esales.com>
2020-08-10 14:00:01 +0200

    OXDEV-3796 Fixed default sorting

--- src/Category/DataType/Sorting.php
+++ src/Category/DataType/Sorting.php
@@ -15,22 +15,14 @@ use TheCodingMachine\GraphQLite\Annotations\Factory;
 final class Sorting extends BaseSorting
 {
     /**
-     * @param array<string, string> $sorting
-     */
-    public function __construct(
-        array $sorting
-    ) {
-        $sorting['oxorder'] = self::SORTING_ASC;
-        parent::__construct($sorting);
-    }
-
-    /**
      * @Factory()
      */
     public static function fromUserInput(
-        ?string $title = BaseSorting::SORTING_DESC
+        ?string $sort  = self::SORTING_ASC,
+        ?string $title = null
     ): self {
         return new self([
+            'oxsort'  => $sort,
             'oxtitle' => $title,
         ]);
     }
